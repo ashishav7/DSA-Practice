5701397b6e8264f11ae68b55ed5e34d801e8b222
ashishav7 <ashish.vyas81@gmail.com>
2025-06-17 23:40:01 +0530

    Sliding Window- Q2) First Negative Number In Every WIndow of Size k

--- src/slidingwindow/SlidingWindowMain.java
+++ src/slidingwindow/SlidingWindowMain.java
@@ -1,15 +1,13 @@
 package slidingwindow;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class SlidingWindowMain {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(10,5,1,0,13,19,18);
+        List<Integer> list = Arrays.asList(-10,5,1,0,-13,19,18);
         int k=3;   //windowSize
         System.out.println(maxSumSubArray(list,k));
-
+        System.out.println(firstNegativeNumberInEveryWindowOfSizeK(list,k));
     }
     private static long maxSumSubArray(List<Integer> list, int k) {
         long sum =0;
@@ -30,5 +28,30 @@ public class SlidingWindowMain {
             r++;
         }
         return maxSum;
+    }
+    private static List<Integer> firstNegativeNumberInEveryWindowOfSizeK(List<Integer> list, int k){
+        int l=0;
+        int r=0;
+        List<Integer> possibleAnswerList = new ArrayList<>();
+        List<Integer> answerList = new ArrayList<>();
+        while(r<list.size()){
+            int element=list.get(r);
+            if(element<0){
+                possibleAnswerList.add(element);
+            }
+            if(r-l+1==k){
+                if(possibleAnswerList.isEmpty()){
+                    answerList.add(0);
+                }else{
+                    answerList.add(possibleAnswerList.getFirst());
+                }
+                if(list.get(l)<0){
+                    possibleAnswerList.removeFirst();
+                }
+                l++;
+            }
+            r++;
+        }
+        return answerList;
     }
 }
